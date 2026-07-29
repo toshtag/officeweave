a59aa10333fda1f7c136209577a67af251ff05b1
Pocket <119549809+toshtag@users.noreply.github.com>
2026-07-29 17:32:31 +0900

    feat: token の発行と無効化の画面を追加する
    発行できるのは自分の token だけとする。
    他人の名前で発行できると、権限の出所が分からなくなる。
    
    発行した値はその場で一度だけ表示する。保存していないため後から確認できない。

---
+++ app/controllers/api_tokens_controller.rb
@@ -0,0 +1,41 @@
+# 外部からの接続に使う token の管理。
+#
+# 発行できるのは自分の token だけとする。
+# 他人の名前で発行できると、権限の出所が分からなくなる。
+class ApiTokensController < ApplicationController
+  before_action :set_api_token, only: %i[destroy]
+
+  def index
+    @api_tokens = Current.user.api_tokens.recent_first
+    @api_token = Current.user.api_tokens.new
+  end
+
+  def create
+    @api_token = Current.user.api_tokens.new(api_token_params)
+    @api_token.organization = current_organization
+
+    if @api_token.save
+      # 値はこの一度だけ表示する。保存していないため、後から確認できない。
+      flash[:issued_token] = @api_token.token
+      redirect_to api_tokens_path, notice: t("api_tokens.created")
+    else
+      @api_tokens = Current.user.api_tokens.recent_first
+      render :index, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @api_token.revoke!
+
+    redirect_to api_tokens_path, notice: t("api_tokens.revoked"), status: :see_other
+  end
+
+  private
+    def set_api_token
+      @api_token = Current.user.api_tokens.find(params[:id])
+    end
+
+    def api_token_params
+      params.expect(api_token: %i[name])
+    end
+end
